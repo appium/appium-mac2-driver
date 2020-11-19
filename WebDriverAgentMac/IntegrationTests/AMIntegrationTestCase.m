86ead8eaa5c9880de7229f47903c37bc82b2ea8d
Mykola Mokhnach <mokhnach@gmail.com>
2020-11-19 18:09:34 +0100

    chore: Add integration tests

---
+++ WebDriverAgentMac/IntegrationTests/AMIntegrationTestCase.m
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "AMIntegrationTestCase.h"
+
+#import "FBConfiguration.h"
+
+@interface AMIntegrationTestCase ()
+@property (nonatomic, strong) XCUIApplication *testedApplication;
+@end
+
+@implementation AMIntegrationTestCase
+
+- (void)setUp
+{
+  [super setUp];
+  FBConfiguration.sharedConfiguration.attributeKeyPathAnalysis = NO;
+  FBConfiguration.sharedConfiguration.automaticScreenshots = NO;
+  self.continueAfterFailure = NO;
+  self.testedApplication = [XCUIApplication new];
+}
+
+- (void)tearDown
+{
+  [super tearDown];
+}
+
+- (void)launchApplication
+{
+  [self.testedApplication launch];
+}
+
+@end
---
+++ WebDriverAgentMac/IntegrationTests/AMSourceTests.m
@@ -0,0 +1,43 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import "AMIntegrationTestCase.h"
+#import "XCUIApplication+AMXml.h"
+
+
+@interface AMSourceTests : AMIntegrationTestCase
+@end
+
+@implementation AMSourceTests
+
+- (void)setUp
+{
+  [super setUp];
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    [self launchApplication];
+  });
+}
+
+- (void)testDescriptionRepresentation
+{
+  NSString *description = self.testedApplication.am_descriptionRepresentation;
+  XCTAssertTrue(description.length > 0);
+}
+
+@end
