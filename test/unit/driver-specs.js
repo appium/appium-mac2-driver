2543907cc9334e1edbe9014bb9bff72c2bb5259b
Mykola Mokhnach <mokhnach@gmail.com>
2020-11-20 15:51:29 +0100

    Update umbrella driver

--- test/unit/driver-specs.js
+++ test/unit/driver-specs.js
@@ -1,9 +1,9 @@
-import GeckoDriver from '../..';
+import Mac2Driver from '../..';
 import chai from 'chai';
 const should = chai.should();
 
-describe('GeckoDriver', function () {
+describe('Mac2Driver', function () {
   it('should exist', function () {
-    should.exist(GeckoDriver);
+    should.exist(Mac2Driver);
   });
 });
