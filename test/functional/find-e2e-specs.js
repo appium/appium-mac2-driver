6405b4387c5b653e5a8461c382d0e28d1f7ca837
Mykola Mokhnach <mokhnach@gmail.com>
2020-11-21 19:53:32 +0100

    Add more location tests

--- test/functional/find-e2e-specs.js
+++ test/functional/find-e2e-specs.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import wd from 'wd';
 import { startServer } from '../..';
 import chaiAsPromised from 'chai-as-promised';
@@ -49,6 +50,52 @@ describe('Mac2Driver - find elements', function () {
     els.length.should.eql(1);
     await els[0].getAttribute('identifier').should.eventually.eql(ACCESSIBILITY_ID);
   });
+
+  it('should find by class name', async function () {
+    const el = await driver.elementByClassName('XCUIElementTypeTextView');
+    el.should.exist;
+  });
+
+  it('should find by multiple by class name', async function () {
+    const els = await driver.elementsByClassName('XCUIElementTypeRulerMarker');
+    els.length.should.be.above(1);
+  });
+
+  it('should find by predicate', async function () {
+    const els = await driver.elements('-ios predicate string', 'elementType == 2');
+    els.length.should.be.above(0);
+    await els[0].getAttribute('elementType').should.eventually.eql('2');
+  });
+
+  it('should find by class chain', async function () {
+    const els = await driver.elements('-ios class chain', '**/XCUIElementTypePopUpButton');
+    els.length.should.be.above(0);
+    await _.first(els).getAttribute('elementType').should.eventually.eql('14');
+    await _.last(els).getAttribute('elementType').should.eventually.eql('14');
+  });
+
+  it('should find by xpath', async function () {
+    const el = await driver.elementByXPath(
+      '//XCUIElementTypePopUpButton[@value="Regular" and @label="type face"]');
+    el.should.exist;
+  });
+
+  it('should find multiple by xpath', async function () {
+    const els = await driver.elementsByXPath('//XCUIElementTypePopUpButton[@enabled="true"]');
+    els.length.should.be.above(1);
+    await _.first(els).getAttribute('elementType').should.eventually.eql('14');
+    await _.last(els).getAttribute('elementType').should.eventually.eql('14');
+  });
+
+  it('should find subelements', async function () {
+    const el = await driver.elementByClassName('XCUIElementTypeRuler');
+    el.should.exist;
+    const subEls = await el.elements('-ios class chain', '*');
+    subEls.length.should.be.above(1);
+    await _.first(subEls).getAttribute('elementType').should.eventually.eql('72');
+    await _.last(subEls).getAttribute('elementType').should.eventually.eql('72');
+  });
+
 });
 
 
