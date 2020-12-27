c219ce75a0063069b7b9a82e064b4042328b2162
Mykola Mokhnach <mokhnach@gmail.com>
2020-12-27 10:28:48 +0100

    chore: Only print serialization error if the request body is not empty

--- WebDriverAgentMac/WebDriverAgentLib/Routing/FBWebServer.m
+++ WebDriverAgentMac/WebDriverAgentLib/Routing/FBWebServer.m
@@ -146,13 +146,18 @@ static NSString *const FBServerURLEndMarker = @"<-ServerURLHere";
   for (Class<FBCommandHandler> commandHandler in commandHandlerClasses) {
     NSArray *routes = [commandHandler routes];
     for (FBRoute *route in routes) {
-      [self.server handleMethod:route.verb withPath:route.path block:^(RouteRequest *request, RouteResponse *response) {
-        NSError *error = nil;
-        NSDictionary *arguments = [NSJSONSerialization JSONObjectWithData:request.body
-                                                                  options:NSJSONReadingMutableContainers
-                                                                    error:&error];
-        if (nil != error) {
-          [FBLogger logFmt:@"%@", error.localizedDescription];
+      [self.server handleMethod:route.verb
+                       withPath:route.path
+                          block:^(RouteRequest *request, RouteResponse *response) {
+        NSDictionary *arguments = nil;
+        if ([request.body length]) {
+          NSError *error = nil;
+          arguments = [NSJSONSerialization JSONObjectWithData:request.body
+                                                      options:NSJSONReadingMutableContainers
+                                                        error:&error];
+          if (nil != error) {
+            [FBLogger logFmt:@"%@", error.localizedDescription];
+          }
         }
         FBRouteRequest *routeParams = [FBRouteRequest
           routeRequestWithURL:request.url
