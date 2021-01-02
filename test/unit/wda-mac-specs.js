f4b06ecf67a805207c33aab763e500b99b4f5d8d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-01-02 02:28:33 -0800

    fix

--- test/unit/wda-mac-specs.js
+++ test/unit/wda-mac-specs.js
@@ -1,9 +1,27 @@
-import Mac2Driver from '../..';
-import chai from 'chai';
-const should = chai.should();
+import WDA_MAC_SERVER from '../../lib/wda-mac';
 
-describe('Mac2Driver', function () {
-  it('should exist', function () {
-    should.exist(Mac2Driver);
+describe('WDAMacServer', function () {
+  describe('_get_proxy_preference', function () {
+    it('should default', function () {
+      WDA_MAC_SERVER._get_proxy_preference({}).should.eql(
+        {scheme: 'http', host: '127.0.0.1', port: 10100, path: ''}
+      );
+    });
+
+    it('should follow WebDriverAgentMacUrl', function () {
+      WDA_MAC_SERVER._get_proxy_preference({
+        WebDriverAgentMacUrl: 'http://customhost:9999'
+      }).should.eql(
+        {scheme: 'http', host: 'customhost', port: 9999, path: ''}
+      );
+    });
+
+    it('should follow WebDriverAgentMacUrl with custom path', function () {
+      WDA_MAC_SERVER._get_proxy_preference({
+        WebDriverAgentMacUrl: 'https://customhost/path'
+      }).should.eql(
+        {scheme: 'https', host: 'customhost', port: 10100, path: '/path'}
+      );
+    });
   });
 });
