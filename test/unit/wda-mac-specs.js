4b2f2d83bbc03957b9eec86b56ced5eeebda9796
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-01-02 11:50:58 -0800

    add docstring

--- test/unit/wda-mac-specs.js
+++ test/unit/wda-mac-specs.js
@@ -1,15 +1,15 @@
 import WDA_MAC_SERVER from '../../lib/wda-mac';
 
 describe('WDAMacServer', function () {
-  describe('_get_proxy_preference', function () {
+  describe('getProxyToInfo', function () {
     it('should default', function () {
-      WDA_MAC_SERVER._get_proxy_preference({}).should.eql(
+      WDA_MAC_SERVER.getProxyToInfo({}).should.eql(
         {scheme: 'http', host: '127.0.0.1', port: 10100, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl', function () {
-      WDA_MAC_SERVER._get_proxy_preference({
+      WDA_MAC_SERVER.getProxyToInfo({
         WebDriverAgentMacUrl: 'http://customhost:9999'
       }).should.eql(
         {scheme: 'http', host: 'customhost', port: 9999, path: ''}
@@ -17,7 +17,7 @@ describe('WDAMacServer', function () {
     });
 
     it('should follow WebDriverAgentMacUrl with custom path', function () {
-      WDA_MAC_SERVER._get_proxy_preference({
+      WDA_MAC_SERVER.getProxyToInfo({
         WebDriverAgentMacUrl: 'https://customhost/path'
       }).should.eql(
         {scheme: 'https', host: 'customhost', port: 10100, path: '/path'}
