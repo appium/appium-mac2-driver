07276ceda21554e14f4229ff475a107f0cf51d0b
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-01-03 01:06:18 -0800

    update test

--- test/unit/wda-mac-specs.js
+++ test/unit/wda-mac-specs.js
@@ -4,19 +4,19 @@ describe('WDAMacServer', function () {
   describe('parseProxyProperties', function () {
     it('should default', function () {
       WDA_MAC_SERVER.parseProxyProperties(undefined).should.eql(
-        {scheme: 'http', host: '127.0.0.1', port: 10100, path: ''}
+        {scheme: 'http', server: '127.0.0.1', port: 10100, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl', function () {
       WDA_MAC_SERVER.parseProxyProperties('http://customhost:9999').should.eql(
-        {scheme: 'http', host: 'customhost', port: 9999, path: ''}
+        {scheme: 'http', server: 'customhost', port: 9999, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl with custom path', function () {
       WDA_MAC_SERVER.parseProxyProperties('https://customhost/path').should.eql(
-        {scheme: 'https', host: 'customhost', port: 10100, path: '/path'}
+        {scheme: 'https', server: 'customhost', port: 10100, path: '/path'}
       );
     });
   });
