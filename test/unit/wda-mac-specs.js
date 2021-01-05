7fbad3928af7fe5d7aecfbccf97806d7d154589a
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-01-04 17:00:16 -0800

    separate should prepare proxy

--- test/unit/wda-mac-specs.js
+++ test/unit/wda-mac-specs.js
@@ -1,21 +1,41 @@
 import WDA_MAC_SERVER from '../../lib/wda-mac';
 
 describe('WDAMacServer', function () {
+  describe('shouldPrepareProxy', function () {
+    beforeEach(function () {
+      WDA_MAC_SERVER.isProxyingToRemoteServer = false;
+      WDA_MAC_SERVER.proxy = null;
+    });
+
+    it('should do with isProxyingToRemoteServer true', function () {
+      WDA_MAC_SERVER.isProxyingToRemoteServer = true;
+      WDA_MAC_SERVER.shouldPrepareProxy().should.be.true;
+    });
+
+    it('should do with no proxy instancce', function () {
+      WDA_MAC_SERVER.shouldPrepareProxy().should.be.true;
+    });
+
+    it('should not do since it has proxy instance WebDriverAgentMacUrl with custom path', function () {
+      WDA_MAC_SERVER.proxy = Object();
+      WDA_MAC_SERVER.shouldPrepareProxy().should.be.false;
+    });
+  });
   describe('parseProxyProperties', function () {
     it('should default', function () {
-      WDA_MAC_SERVER.parseProxyProperties(undefined).should.eql(
+      WDA_MAC_SERVER.parseProxyProperties({}).should.eql(
         {scheme: 'http', server: '127.0.0.1', port: 10100, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl', function () {
-      WDA_MAC_SERVER.parseProxyProperties('http://customhost:9999').should.eql(
+      WDA_MAC_SERVER.parseProxyProperties({ webDriverAgentMacUrl: 'http://customhost:9999' }).should.eql(
         {scheme: 'http', server: 'customhost', port: 9999, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl with custom path', function () {
-      WDA_MAC_SERVER.parseProxyProperties('https://customhost/path').should.eql(
+      WDA_MAC_SERVER.parseProxyProperties({ webDriverAgentMacUrl: 'https://customhost/path' }).should.eql(
         {scheme: 'https', server: 'customhost', port: 10100, path: '/path'}
       );
     });
