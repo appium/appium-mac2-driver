7f14e8dec2214a5f96628256a841ec7b82b650ef
Jonathan Lipps <jlipps@gmail.com>
2022-01-05 10:00:37 -0800

    fix: make sure to transpile index.js (fix #109)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ let boilerplate = require('@appium/gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-mac2-driver',
+  files: ['index.js', 'lib/**/*.js', 'test/**/*.js', '!gulpfile.js'],
   coverage: {
     files: ['./test/unit/**/*-specs.js', '!./test/functional/**'],
     verbose: true
