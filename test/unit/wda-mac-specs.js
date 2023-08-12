c9085034ebe57ee743acb18da5236af4563873f7
Mykola Mokhnach <mokhnach@gmail.com>
2023-08-13 00:06:37 +0200

    feat: Switch babel to typescript (#216)

--- test/unit/wda-mac-specs.js
+++ test/unit/wda-mac-specs.js
@@ -8,19 +8,19 @@ describe('WDAMacServer', function () {
   describe('parseProxyProperties', function () {
     it('should default', function () {
       WDA_MAC_SERVER.parseProxyProperties({}).should.eql(
-        {scheme: 'http', server: '127.0.0.1', port: 10100, path: ''}
+        {scheme: 'http', host: '127.0.0.1', port: 10100, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl', function () {
       WDA_MAC_SERVER.parseProxyProperties({ webDriverAgentMacUrl: 'http://customhost:9999' }).should.eql(
-        {scheme: 'http', server: 'customhost', port: 9999, path: ''}
+        {scheme: 'http', host: 'customhost', port: 9999, path: ''}
       );
     });
 
     it('should follow WebDriverAgentMacUrl with custom path', function () {
       WDA_MAC_SERVER.parseProxyProperties({ webDriverAgentMacUrl: 'https://customhost/path' }).should.eql(
-        {scheme: 'https', server: 'customhost', port: 10100, path: '/path'}
+        {scheme: 'https', host: 'customhost', port: 10100, path: '/path'}
       );
     });
 
