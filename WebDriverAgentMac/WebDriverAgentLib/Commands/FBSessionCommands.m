04533d796a27d56834c22a9e15c511bf78d588ab
Mykola Mokhnach <mokhnach@gmail.com>
2024-04-18 09:12:03 +0200

    Add an error message

--- WebDriverAgentMac/WebDriverAgentLib/Commands/FBSessionCommands.m
+++ WebDriverAgentMac/WebDriverAgentLib/Commands/FBSessionCommands.m
@@ -91,6 +91,14 @@ const static NSString *CAPABILITIES_KEY = @"capabilities";
     XCUIApplication *app = nil != appPath 
       ? [[XCUIApplication alloc] initWithURL:[NSURL fileURLWithPath:appPath]]
       : [[XCUIApplication alloc] initWithBundleIdentifier:bundleID];
+    if (nil != bundleID && nil != appPath) {
+      NSString *realBundleID = app.am_bundleID;
+      if (![realBundleID isEqualToString:bundleID]) {
+        NSString *message = [NSString stringWithFormat:@"The bundle identifier %@ of the '%@' does not match to the one provided in capabilities: %@", realBundleID, appPath, bundleID];
+        return FBResponseWithStatus([FBCommandStatus sessionNotCreatedError:message
+                                                                  traceback:nil]);
+      }
+    }
     session = [FBSession initWithApplication:app];
     if (noReset && app.state > XCUIApplicationStateNotRunning) {
       [app activate];
