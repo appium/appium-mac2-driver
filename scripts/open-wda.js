f5cfa8104b11d569a19122482543e90dfa2e4735
Mykola Mokhnach <mokhnach@gmail.com>
2024-08-01 18:33:49 +0200

    feat: Add open-wda script (#312)

---
+++ scripts/open-wda.js
@@ -0,0 +1,14 @@
+const path = require('node:path');
+const {exec} = require('teen_process');
+const {logger} = require('@appium/support');
+const XCODEPROJ_NAME = 'WebDriverAgentMac.xcodeproj';
+
+const log = logger.getLogger('WDA');
+
+async function openWda() {
+  const dstPath = path.resolve(__dirname, '..', 'WebDriverAgentMac', XCODEPROJ_NAME);
+  log.info(`Opening '${dstPath}'`);
+  await exec('open', [dstPath]);
+}
+
+(async () => await openWda())();
