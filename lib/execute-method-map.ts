4703e097277f167e7cc48cae611a7839b8ece417
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-02-01 10:04:26 -0800

    macosClick

--- lib/execute-method-map.ts
+++ lib/execute-method-map.ts
@@ -1,17 +1,29 @@
 import { ExecuteMethodMap } from "@appium/types";
 
 export const executeMethodMap = {
-    'macos: setValue': {
-      command: 'macosSetValue',
-      params: {
-        required: [
-            'elementId'
-        ],
-        optional: [
-          'value',
-          'text',
-          'keyModifierFlags',
-        ],
-      },
+  'macos: setValue': {
+    command: 'macosSetValue',
+    params: {
+      required: [
+          'elementId'
+      ],
+      optional: [
+        'value',
+        'text',
+        'keyModifierFlags',
+      ],
     },
-} as const satisfies ExecuteMethodMap<any>;
+  },
+  'macos: click': {
+    command: 'macosClick',
+    params: {
+      required: [
+          'elementId'
+      ],
+      optional: [
+        'x',
+        'y',
+        'keyModifierFlags',
+      ],
+    },
+  },} as const satisfies ExecuteMethodMap<any>;
