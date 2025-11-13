3abd68d967464f8dc8e9ce8f429546479c4b669d
Mykola Mokhnach <mokhnach@gmail.com>
2025-11-13 20:04:34 +0100

    chore: Migrate driver.js to typescript

--- lib/driver.ts
+++ lib/driver.ts
@@ -1,7 +1,20 @@
 import _ from 'lodash';
+import type {
+  RouteMatcher,
+  HTTPMethod,
+  HTTPBody,
+  DefaultCreateSessionResult,
+  DriverData,
+  InitialOpts,
+  StringRecord,
+  ExternalDriver,
+  DriverCaps,
+  DriverOpts,
+  W3CDriverCaps,
+} from '@appium/types';
 import { BaseDriver, DeviceSettings } from 'appium/driver';
-import { WDA_MAC_SERVER } from './wda-mac';
-import { desiredCapConstraints } from './desired-caps';
+import { WDA_MAC_SERVER, type WDAMacServer } from './wda-mac';
+import MAC2_CONSTRAINTS, {type Mac2Constraints} from './constraints';
 import * as appManagemenetCommands from './commands/app-management';
 import * as appleScriptCommands from './commands/applescript';
 import * as executeCommands from './commands/execute';
@@ -12,13 +25,12 @@ import * as recordScreenCommands from './commands/record-screen';
 import * as screenshotCommands from './commands/screenshots';
 import * as sourceCommands from './commands/source';
 import * as clipboardCommands from './commands/clipboard';
+import * as nativeScreenRecordingCommands from './commands/native-record-screen';
 import log from './logger';
 import { newMethodMap } from './method-map';
 import { executeMethodMap } from './execute-method-map';
-import * as nativeScreenRecordingCommands from './commands/native-record-screen';
 
-/** @type {import('@appium/types').RouteMatcher[]} */
-const NO_PROXY = [
+const NO_PROXY: RouteMatcher[] = [
   ['GET', new RegExp('^/session/[^/]+/appium')],
   ['POST', new RegExp('^/session/[^/]+/appium')],
   ['POST', new RegExp('^/session/[^/]+/element/[^/]+/elements?$')],
@@ -29,23 +41,22 @@ const NO_PROXY = [
   ['POST', new RegExp('^/session/[^/]+/timeouts$')],
 ];
 
-export class Mac2Driver extends BaseDriver {
-  /** @type {boolean} */
-  isProxyActive;
-
-  /** @type {import('./wda-mac').WDAMacServer} */
-  wda;
-
-  /** @type {nativeScreenRecordingCommands.NativeVideoChunksBroadcaster} */
-  _videoChunksBroadcaster;
+export class Mac2Driver
+  extends BaseDriver<Mac2Constraints, StringRecord>
+  implements ExternalDriver<Mac2Constraints, string, StringRecord>
+{
+  private isProxyActive: boolean;
+  private _wda: WDAMacServer | null;
+  _videoChunksBroadcaster: nativeScreenRecordingCommands.NativeVideoChunksBroadcaster;
+  _screenRecorder: recordScreenCommands.ScreenRecorder | null;
+  public proxyReqRes: (...args: any) => any;
 
   static newMethodMap = newMethodMap;
   static executeMethodMap = executeMethodMap;
 
-  constructor (opts = {}) {
-    // @ts-ignore TODO: Make opts typed
+  constructor(opts: InitialOpts = {} as InitialOpts) {
     super(opts);
-    this.desiredCapConstraints = desiredCapConstraints;
+    this.desiredCapConstraints = _.cloneDeep(MAC2_CONSTRAINTS);
     this.locatorStrategies = [
       'id',
       'name',
@@ -65,109 +76,128 @@ export class Mac2Driver extends BaseDriver {
     this.settings = new DeviceSettings({}, this.onSettingsUpdate.bind(this));
   }
 
-  async onSettingsUpdate (key, value) {
-    return await this.wda.proxy.command('/appium/settings', 'POST', {
+  async onSettingsUpdate(key: string, value: unknown): Promise<void> {
+    if (!this._wda) {
+      return;
+    }
+    return await this._wda.proxy.command('/appium/settings', 'POST', {
       settings: {[key]: value}
     });
   }
 
-  resetState () {
-    // @ts-ignore This is ok
-    this.wda = null;
-    this.proxyReqRes = null;
-    this.isProxyActive = false;
-    this._videoChunksBroadcaster = new nativeScreenRecordingCommands.NativeVideoChunksBroadcaster(
-      this.eventEmitter, this.log
-    );
-    this._screenRecorder = null;
+  get wda(): WDAMacServer {
+    if (!this._wda) {
+      throw new Error('WDA server is not initialized');
+    }
+    return this._wda;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  proxyActive (sessionId) {
+  override proxyActive(sessionId: string): boolean {
     return this.isProxyActive;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  getProxyAvoidList (sessionId) {
+  override getProxyAvoidList(sessionId: string): RouteMatcher[] {
     return NO_PROXY;
   }
 
-  canProxy () {
+  override canProxy(): boolean {
     return true;
   }
 
-  /**
-   *
-   * @param {string} url
-   * @param {import('@appium/types').HTTPMethod} method
-   * @param {any} body
-   * @returns {Promise<any>}
-   */
-  async proxyCommand (url, method, body = null) {
-    return await this.wda.proxy.command(url, method, body);
+  async proxyCommand(url: string, method: HTTPMethod, body: HTTPBody = null): Promise<any> {
+    if (!this._wda) {
+      throw new Error('WDA server is not initialized');
+    }
+    return await this._wda.proxy.command(url, method, body);
   }
 
-  // @ts-ignore We know WDA response should be ok
-  async getStatus () {
-    return await this.wda.proxy.command('/status', 'GET');
+  override async getStatus(): Promise<any> {
+    if (!this._wda) {
+      throw new Error('WDA server is not initialized');
+    }
+    return await this._wda.proxy.command('/status', 'GET');
   }
 
   // needed to make image plugin work
-  async getWindowRect () {
-    return await this.wda.proxy.command('/window/rect', 'GET');
+  async getWindowRect(): Promise<any> {
+    if (!this._wda) {
+      throw new Error('WDA server is not initialized');
+    }
+    return await this._wda.proxy.command('/window/rect', 'GET');
   }
 
-  // @ts-ignore TODO: make args typed
-  async createSession (...args) {
-    // @ts-ignore TODO: make args typed
-    const [sessionId, caps] = await super.createSession(...args);
-    this.wda = WDA_MAC_SERVER;
+  override async createSession(
+    w3cCaps1: W3CMac2DriverCaps,
+    w3cCaps2?: W3CMac2DriverCaps,
+    w3cCaps3?: W3CMac2DriverCaps,
+    driverData?: DriverData[]
+  ): Promise<DefaultCreateSessionResult<Mac2Constraints>> {
+    const [sessionId, caps] = await super.createSession(w3cCaps1, w3cCaps2, w3cCaps3, driverData);
+    this._wda = WDA_MAC_SERVER;
+    this.caps = caps as Mac2DriverCaps;
+    this.opts = this.opts as Mac2DriverOpts;
     try {
-      if (caps.prerun) {
-        if (!_.isString(caps.prerun.command) && !_.isString(caps.prerun.script)) {
+      const prerun = caps.prerun as PrerunCapability | undefined;
+      if (prerun) {
+        if (!_.isString(prerun.command) && !_.isString(prerun.script)) {
           throw new Error(`'prerun' capability value must either contain ` +
             `'script' or 'command' entry of string type`);
         }
         log.info('Executing prerun AppleScript');
-        const output = await this.macosExecAppleScript(caps.prerun);
+        const output = await this.macosExecAppleScript(
+          prerun.script,
+          undefined,
+          prerun.command
+        );
         if (_.trim(output)) {
           log.info(`Prerun script output: ${output}`);
         }
       }
-      await this.wda.startSession(caps, {
+      await this._wda.startSession(caps, {
         reqBasePath: this.basePath,
       });
-    } catch (e) {
+    } catch (e: any) {
       await this.deleteSession();
       throw e;
     }
-    this.proxyReqRes = this.wda.proxy.proxyReqRes.bind(this.wda.proxy);
+    this.proxyReqRes = this.wda.proxy.proxyReqRes.bind(this._wda.proxy);
     this.isProxyActive = true;
     return [sessionId, caps];
   }
 
-  async deleteSession () {
+  override async deleteSession(): Promise<void> {
     await this._screenRecorder?.stop(true);
     if (this._videoChunksBroadcaster.hasPublishers) {
-      try {
-        await this.wda.proxy.command('/wda/video/stop', 'POST', {});
-      } catch {}
+      if (this._wda) {
+        try {
+          await this.wda.proxy.command('/wda/video/stop', 'POST', {});
+        } catch {}
+      }
       await this._videoChunksBroadcaster.shutdown(5000);
     }
-    await this.wda.stopSession();
+    if (this._wda) {
+      await this.wda.stopSession();
+    }
 
-    if (this.opts.postrun) {
-      if (!_.isString(this.opts.postrun.command) && !_.isString(this.opts.postrun.script)) {
+    const postrun = this.opts.postrun as PostrunCapability | undefined;
+    if (postrun) {
+      if (!_.isString(postrun.command) && !_.isString(postrun.script)) {
         log.error(`'postrun' capability value must either contain ` +
           `'script' or 'command' entry of string type`);
       } else {
         log.info('Executing postrun AppleScript');
         try {
-          const output = await this.macosExecAppleScript(this.opts.postrun);
+          const output = await this.macosExecAppleScript(
+            postrun.script,
+            undefined,
+            postrun.command
+          );
           if (_.trim(output)) {
             log.info(`Postrun script output: ${output}`);
           }
-        } catch (e) {
+        } catch (e: any) {
           log.error(e.message);
         }
       }
@@ -176,6 +206,15 @@ export class Mac2Driver extends BaseDriver {
     this.resetState();
 
     await super.deleteSession();
+  }
+
+  private resetState(): void {
+    this._wda = null;
+    this.isProxyActive = false;
+    this._videoChunksBroadcaster = new nativeScreenRecordingCommands.NativeVideoChunksBroadcaster(
+      this.eventEmitter, this.log
+    );
+    this._screenRecorder = null;
   }
 
   macosLaunchApp = appManagemenetCommands.macosLaunchApp;
@@ -224,3 +263,17 @@ export class Mac2Driver extends BaseDriver {
 }
 
 export default Mac2Driver;
+
+interface PrerunCapability {
+  command?: string;
+  script?: string;
+}
+
+interface PostrunCapability {
+  command?: string;
+  script?: string;
+}
+
+type Mac2DriverOpts = DriverOpts<Mac2Constraints>;
+type Mac2DriverCaps = DriverCaps<Mac2Constraints>;
+type W3CMac2DriverCaps = W3CDriverCaps<Mac2Constraints>;
